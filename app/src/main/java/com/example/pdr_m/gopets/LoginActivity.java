18a36c33ff3106cbc3385ccb2d6547ae47bd5240
pdr_m <pdr_mdrs@hotmail.com>
2017-06-22 11:14:33 -0300

    Test Commit

--- app/src/main/java/com/example/pdr_m/gopets/LoginActivity.java
+++ app/src/main/java/com/example/pdr_m/gopets/LoginActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+    /**
+     * onCreate method
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
